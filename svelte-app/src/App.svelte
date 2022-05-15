b98f24ce5331e6d01859ffcfa930ce501c19776a
MikeGulik <mgulik@grubbrr.com>
2022-05-15 16:33:28 -0400

    converted

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,26 +1,17 @@
 <script>
-  import './styles.scss';
-  import { Router, Link, Route } from 'svelte-routing';
-  import About from './About.svelte';
-  import Products from './products/Products.svelte';
-
-  import { HeaderBar, NavBar, PageNotFound, Redirect } from './components';
+  import { Router, Route } from 'svelte-routing';
+  import Data from './routes/data.svelte';
+  import Lost from './routes/lost.svelte';
 
   export let url = '';
 </script>
 
-<HeaderBar />
 <div class="section columns">
   <Router {url}>
-    <NavBar />
     <main class="column">
       <div>
-        <Route path="/">
-          <Redirect path="/products" />
-        </Route>
-        <Route path="/products" component={Products} />
-        <Route path="/about" component={About} />
-        <Route path="**" component={PageNotFound} />
+        <Route path="/:data" component={Data} />
+        <Route path="**" component={Lost} />
       </div>
     </main>
   </Router>
