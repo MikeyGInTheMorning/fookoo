1dec37615ba64e78b405034e8b7db092edf39eef
Michael <mikegulik@gmail.com>
2022-05-16 00:35:46 -0400

    talking to blob and retrieving sayings

--- Fookoo.Web/Fookoo.Web/GetSayingsFunction.cs
+++ Fookoo.Web/Fookoo.Web/GetSayingsFunction.cs
@@ -11,25 +11,63 @@ using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using System.Text;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Fookoo.Web
 {
     public static class GetSayingsFunction
     {
-        [FunctionName("GetSayingFunction")]
+        [FunctionName("GetSaying")]
         public static async Task<IActionResult> GetSaying(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get",  Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            var hash = req.Query["hash"];
+            var isHashValid = !string.IsNullOrEmpty(hash);
 
-            string name = req.Query["hash"];
-
-        
             await GetAzureServices();
-            await UploadToBlob();
-            var responseMessage = "\n\n" + await ListBlobs();
-            await DownloadFromBlob();
+            var sayings = await DownloadFromBlob();
+            var isSayingsValid = sayings is not null || sayings.Count != 0;
+
+            if (!isSayingsValid || !isHashValid)
+                return new OkObjectResult(new Saying());
+
+            var saying =  sayings.FirstOrDefault(a => a.Hash.ToLower() == hash.ToString().ToLower()) ?? new Saying();
+
+            return new OkObjectResult(saying);
+        }
+
+        public class AddSayingRequest
+        {
+            [JsonProperty("sentence")]
+            public string Sentence { get; set; }
+        }
+
+        [FunctionName("AddSaying")]
+        public static async Task<IActionResult> AddSaying(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+    ILogger log)
+        {
+            await GetAzureServices();
+            var sayings = await DownloadFromBlob();
+
+            if (sayings is not null && sayings.Count == 10000)
+                return new BadRequestObjectResult("Exceeeded custom syaing cap, try again tomorrow!");
+
+
+            var content = await new StreamReader(req.Body).ReadToEndAsync();
+            var body = JsonConvert.DeserializeObject<AddSayingRequest>(content);
+
+            var newId = Guid.NewGuid().ToString();
+            sayings.Add(new()
+            {
+                Hash = newId,
+                Sentence = body.Sentence
+            });
+
+            await UploadToBlob(sayings);
+
+            var responseMessage = $"Saved Saying! Your Hash is {newId}";
             return new OkObjectResult(responseMessage);
         }
 
@@ -70,41 +108,37 @@ namespace Fookoo.Web
             return retVal;
         }
 
-        private static async Task UploadToBlob()
+        private static async Task UploadToBlob(object uploadObject)
         {
             Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
-
-            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(myDataObject)))
+            var data = JsonConvert.SerializeObject(uploadObject);
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(data)))
             {
                 await blobClient.UploadAsync(ms, overwrite: true);
             }
         }
 
-        private static async Task DownloadFromBlob()
+        private static async Task<List<Saying>> DownloadFromBlob()
         {
             Console.WriteLine("\nDownloading blob to\n\t{0}\n", blobName);
 
             // Download the blob's contents and save it to a file
-            using (var stream = new MemoryStream())
-            {
-                await blobClient.DownloadToAsync(stream);
-                stream.Position = 0;//resetting stream's position to 0
-                var serializer = new JsonSerializer();
+            using var stream = new MemoryStream();
+            await blobClient.DownloadToAsync(stream);
+            stream.Position = 0;//resetting stream's position to 0
+            var serializer = new JsonSerializer();
 
-                using var sr = new StreamReader(stream);
-                using var jsonTextReader = new JsonTextReader(sr);
-                var result = serializer.Deserialize<List<Saying>>(jsonTextReader);
+            using var sr = new StreamReader(stream);
+            using var jsonTextReader = new JsonTextReader(sr);
 
-                Console.WriteLine("\nDownloading blob to\n\t{0}\n", result);
-            }
+            var result = serializer.Deserialize<List<Saying>>(jsonTextReader) ?? new();
+
+            Console.WriteLine("\nDownloading blob to\n\t{0}\n", result);
+
+            return result;
         }
 
-        public class Root
-        {
-            [JsonProperty("sayings")]
-            List<Saying> Sayings { get; set; }
-        }
-
+        [JsonObject]
         public class Saying
         {
             [JsonProperty("hash")]
@@ -114,22 +148,19 @@ namespace Fookoo.Web
             public string Sentence { get; set; }
         }
 
-
-
-        private static string myDataObject = @"
-{
-  ""sayings"": [
-    {
-      ""hash"": ""1"",
-      ""saying"": ""I Love You.""
-    },
-    {
-      ""hash"": ""2"",
-      ""saying"": ""I Love You 2.""
-    }
-  ]
-}
-            ";
+        private static List<Saying> testObj = new List<Saying>()
+        {
+            new()
+            {
+                Hash ="1",
+                Sentence = "I Love You."
+            },
+            new()
+            {
+                Hash ="2",
+                Sentence = "I Love You 2."
+            }
+        };
     }
 }
 
